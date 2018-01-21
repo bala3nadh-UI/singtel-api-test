9f02f3d7bbd6428deabe292267ee4786086f097f
Bala Trinadh <30519317+bala3nadh-UI@users.noreply.github.com>
2018-01-21 20:55:05 +0800

    final version

--- gruntfile.js
+++ gruntfile.js
@@ -1,17 +1,39 @@
 module.exports = function(grunt){
     
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
+      pkg: grunt.file.readJSON('package.json'),
 
-     uglify: {
-      options: {
-        mangle: true
+      uglify: {
+        options: {
+          mangle: true
+        },
+        build: {
+          src: "js/dev.js",
+          dest: "js/prod-min.js"
+        }
       },
-      build: {
-        src: "js/dev.js",
-        dest: "js/prod-min.js"
+      cssmin: { // minifying css task
+        dist: {
+          files: {
+            'cssStyles/styles.min.css': 'cssStyles/styles.css'
+          }
+        }
+      },
+      compass: {
+          dist: {
+              options: {
+                  sassDir: 'sassStyles',
+                  cssDir: 'sassStyles',
+                  environment: 'development',
+                  outputStyle: 'compressed'
+              }
+          }
       }
-    }
     });
     grunt.loadNpmTasks("grunt-contrib-uglify");
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-compass');
+    grunt.registerTask('default', ['sass', 'cssmin', 'compass']);
+
 };
--- js/dev.js
+++ js/dev.js
@@ -1,6 +1,10 @@
-    json1 = new Object;
-    var selctedIndex1 = 0;
+"use strict";
 
+    var jsonFromApi = new Object;
+    var selctedDDIndex = 0;
+    var spanElem = document.createElement("span");
+    spanElem.innerHTML = "&nbsp;";
+    
     var HttpClient = function() {
         this.get = function(aUrl, aCallback) {
             var anHttpRequest = new XMLHttpRequest();
@@ -16,33 +20,36 @@
 
     var client = new HttpClient();
     client.get('http://pb-api.herokuapp.com/bars', function(response) {
-        json1 = JSON.parse(response);
+        jsonFromApi = JSON.parse(response);
         startIt();
     });
 
     function startIt() {
         //Create an Dropdown from JSON.
-        dropdownElem = document.createElement("select");
+        var dropdownElem = document.createElement("select");
         dropdownElem.id = "dd1";
-        for (i=0; i<json1.bars.length; i++) {
+        var i = 0;
+        for (i=0; i<jsonFromApi.bars.length; i++) {
             var op = new Option();
             op.value = i+1;
             op.text = "#Progress" + (i+1);
             dropdownElem.options.add(op);
 
             var percentId = "percent"+(i+1);
-            document.getElementById("progressBars").innerHTML += '<div class="progress"><div id=' + percentId + ' class="progress-bar" role="progressbar" style="width: ' + json1.bars[i] + '%"' + '></div></div>';
+            document.getElementById("progressBars").innerHTML += '<div class="progress"><div id=' + percentId + ' class="progress-bar" role="progressbar" style="width: ' + jsonFromApi.bars[i] + '%"' + '></div></div>';
         }
         document.getElementById("footer").appendChild(dropdownElem);
+        document.getElementById("dd1").classList.add('selectpicker');
         document.getElementById("dd1").addEventListener("change", intializePrgBar);
-
+        document.getElementById("footer").appendChild(spanElem);
+        
         //Create an Input type from JSON.  
-        for(i=0; i<json1.buttons.length; i++){
+        for(i=0; i<jsonFromApi.buttons.length; i++){
           var btn1 = document.createElement("BUTTON"); 
           btn1.id ='btn' + i;  
-          btn1.innerHTML = json1.buttons[i]; 
-          btn1.dataset.val = json1.buttons[i]; 
-          btn1.dataset.cnt = json1.buttons[i];
+          btn1.innerHTML = jsonFromApi.buttons[i]; 
+          btn1.dataset.val = jsonFromApi.buttons[i]; 
+          btn1.dataset.cnt = jsonFromApi.buttons[i];
           //btn1.setAttribute("id", "btn1");
           var foo = document.getElementById("footer"); 
           foo.appendChild(btn1);
@@ -55,14 +62,14 @@
         console.log(this);
         var e = this;
         var selOptVal = e.options[e.selectedIndex].value;
-        selctedIndex1 = e.selectedIndex;
+        selctedDDIndex = e.selectedIndex;
         var id = 'percent'+(selOptVal);
         var progressTag = document.getElementById(id);
-        progressTag.style.width = json1.bars[e.selectedIndex] + "%";
+        progressTag.style.width = jsonFromApi.bars[e.selectedIndex] + "%";
     }
 
     function changeWidth(){
-        var id = "percent" + (selctedIndex1+1);
+        var id = "percent" + (selctedDDIndex+1);
         var progressTag = document.getElementById(id);
         //var countTag = this;
         var count = parseInt(progressTag.style.width);
@@ -74,12 +81,12 @@
                 count = count + amt;
             }        
         }else {
-            if(count <= parseInt(json1.limit)){
+            if(count <= parseInt(jsonFromApi.limit)){
                 count = count + amt;
             }
         }
-        if(count > parseInt(json1.limit)){
-            count = parseInt(json1.limit);
+        if(count > parseInt(jsonFromApi.limit)){
+            count = parseInt(jsonFromApi.limit);
         } else if(count < 0) {
             count = 0;
         }
@@ -91,7 +98,7 @@
             progressTag.classList.remove('progress-bar-success');
             progressTag.classList.remove('progress-bar-danger');
             progressTag.classList.add('progress-bar-warning');
-        } else if (count >= 80 && count < parseInt(json1.limit)){
+        } else if (count >= 80 && count < parseInt(jsonFromApi.limit)){
             progressTag.classList.remove('progress-bar-warning');
             progressTag.classList.remove('progress-bar-success');
             progressTag.classList.add('progress-bar-danger');
